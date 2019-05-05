c6a89959c2157d9f34015372d562d2b3a3089e07
Edward Iga Kigongo <igakigongo@gmail.com>
2019-05-05 17:45:23 +0200

    Replace multiple css calls in index.html with single call to bundled css for devices with resolution _576px

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,11 @@
-const path = require('path');
+const glob = require('glob');
 const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
 	entry: {
-		_576: ["./assets/css/_576px/app.css",
-			"./assets/css/_576px/apps-gear-tech.css",
-			"./assets/css/_576px/cover-articles.css",
-			"./assets/css/_576px/footer.css",
-			"./assets/css/_576px/latest-deals.css",
-			"./assets/css/_576px/latest-funding.css",
-			"./assets/css/_576px/latest-news.css"
-		]
+		_576: glob.sync('./assets/css/_576px/*.css')
 	},
 
 	module: {
