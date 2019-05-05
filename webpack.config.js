a31ecd9e13548b28cb8e8a07eda8ae4b6221a638
Edward Iga Kigongo <igakigongo@gmail.com>
2019-05-05 17:11:34 +0200

    Intial webpack config with bundling of files for devices with resolution _576px

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,35 @@
 const path = require('path');
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+	entry: {
+		_576: ["./assets/css/_576px/app.css",
+			"./assets/css/_576px/apps-gear-tech.css",
+			"./assets/css/_576px/cover-articles.css",
+			"./assets/css/_576px/footer.css",
+			"./assets/css/_576px/latest-deals.css",
+			"./assets/css/_576px/latest-funding.css",
+			"./assets/css/_576px/latest-news.css"
+		]
+	},
+
+	module: {
+		rules: [{
+			test: /\.css$/,
+			use: [MiniCssExtractPlugin.loader, 'css-loader'],
+		}],
+	},
+
+	optimization: {
+		minimizer: [new OptimizeCSSAssetsPlugin({})],
+	},
+
+	plugins: [
+		new FixStyleOnlyEntriesPlugin(),
+		new MiniCssExtractPlugin({
+			filename: "[name].[chunkhash:8].css"
+		})
+	]
 };
